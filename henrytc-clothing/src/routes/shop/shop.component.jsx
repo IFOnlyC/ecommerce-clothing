edf1372c8ee88ca90a57ff86cfc2185b6f9faf89
Tianhao Chen <tc30@illinois.edu>
2023-05-04 20:19:17 -0700

    using Asynchronous Redux: redux-thunk to migrate async fetch categories

--- henrytc-clothing/src/routes/shop/shop.component.jsx
+++ henrytc-clothing/src/routes/shop/shop.component.jsx
@@ -5,8 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/categories.action';
+import { fetchCategoriesStartAsync } from '../../store/categories/categories.action';
 
 import './shop.style.scss';
 
@@ -14,11 +13,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    };
-    getCategoriesMap();
+    dispatch(fetchCategoriesStartAsync());
   }, []);
 
   return (
--- henrytc-clothing/src/store/store.js
+++ henrytc-clothing/src/store/store.js
@@ -3,6 +3,7 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 // import { loggerMiddleware } from './middleware/logger';
 
 import { rootReducer } from './root-reducer';
@@ -12,14 +13,15 @@ import { rootReducer } from './root-reducer';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
